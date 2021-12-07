2b0fed65f8da9d8904cb26895e89eeff4ec18db3
aanper <mail@s3f.ru>
2021-12-07 20:45:21 +0300

    implement control commands and responses

--- scripts/boot/bydefault/start.py
+++ scripts/boot/bydefault/start.py
@@ -5,6 +5,7 @@ import signal
 import getpass
 from time import sleep, time
 import subprocess
+import threading
 import json
 
 import socket
@@ -22,21 +23,54 @@ except ImportError:
     import os
     DEVNULL = open(os.devnull, 'wb')
 
-def setInterval(interval):
-    def decorator(function):
-        def wrapper(*args, **kwargs):
-            stopped = threading.Event()
+time_diff = None
 
-            def loop(): # executed in another thread
-                while not stopped.wait(interval): # until stopped
-                    function(*args, **kwargs)
+def sync_time(host):
+    global time_diff
 
-            t = threading.Thread(target=loop)
-            t.daemon = True # stop if the program exits
-            t.start()
-            return stopped
-        return wrapper
-    return decorator
+    try:
+        ntp_client = ntplib.NTPClient()
+        ntp_response = ntp_client.request(host, version=3)
+    except:
+        print("ntp server not available")
+        return False
+
+    time_diff = time() - ntp_response.tx_time
+
+    print("time diff:", ntp_response.tx_time, time(), time_diff)
+    return True
+
+def get_server_time():
+    if time_diff is None:
+        return None
+
+    return time() - time_diff
+
+def schedule_play(controller, schedule_time):
+    stopped = threading.Event()
+
+    if get_server_time() is None:
+        print("no server time")
+        return None
+
+    print("shedule to", schedule_time)
+
+    def waiter():
+        while True:
+            d = schedule_time - get_server_time()
+            # print("wait", d)
+            if d < 0:
+                print("sheduled play")
+                controller.play()
+                break
+            if stopped.wait(0.005):
+                print("shedule canceled")
+                break
+
+    t = threading.Thread(target=waiter)
+    t.daemon = True # stop if the program exits
+    t.start()
+    return stopped
 
 player = None
 def run_omx(param):
@@ -79,61 +113,51 @@ screen = get_monitors()[0]
 screen_size = (screen.width, screen.height)
 print("screen size:", screen_size)
 
-time_diff = None
+client = None
 
-def sync_time(host):
-    global time_diff
+def send_message(topic, message, qos=0):
+    if client is None:
+        print("no client")
+        return
+    payload = json.dumps(message)
+    full_topic = f"/s/{my_id}/{topic}"
+    print(f"send {full_topic}: {payload}")
 
-    try:
-        ntp_client = ntplib.NTPClient()
-        ntp_response = ntp_client.request(host, version=3)
-    except:
-        print("ntp server not available")
-        return None
+    client.publish(full_topic, payload=payload, qos=qos, retain=False)
 
-    time_diff = time() - ntp_response.tx_time
-
-    print("time diff:", ntp_response.tx_time, time(), time_diff)
-    return (())
-
-def get_server_time():
-    return time() - time_diff
-
-def on_connect(client, userdata, flags, rc):
+def on_connect(_client, _userdata, _flags, _rc):
     print("connected to broker")
 
-def on_message(client, userdata, msg):
+scheduler = None
+def handle_message(msg):
     global player
+    global scheduler
 
     print("topic:", msg.topic)
     topic = msg.topic.split("/")[1:]
 
     if len(topic) < 3:
-        print("topic not in layout")
-        return
+        return (("err", "topic not in layout"))
 
     if topic[0] != "m":
-        print("non-slave topic")
-        return
+        return (("err", "non-slave topic"))
 
     if topic[1] != my_id:
-        print("topic not for you")
-        return
+        return (("err", "topic not for you"))
 
     method = topic[2]
 
     try:
         message = json.loads(msg.payload)
     except:
-        print("non-json message")
-        return
+        return (("err", "non-json message"))
 
     print("message:", message)
 
-    if method == "sync" and "host" in message:
-        sync_time(message["host"])
+    if method == "sync" and type(message) == str:
+        return (("sync", sync_time(message)))
 
-    if method == "run" and type(message) == list:
+    elif method == "run" and type(message) == list:
         if player is not None:
             print("stop/remove prev video")
             os.killpg(os.getpgid(player[1].pid), signal.SIGTERM)
@@ -141,38 +165,84 @@ def on_message(client, userdata, msg):
             player = None
 
         player = run_omx(message)
+        return None
 
-    if method == "g" and "c" in message and "p" in message and "s" in message:
+    elif method == "g" and "c" in message and "p" in message and "s" in message:
         if player is None:
-            print("no active player")
-            return
+            return (("err", "no active player"))
 
         try:
             set_player_geometry(
                 player[0], player[2], screen_size, message["c"], message["p"], message["s"]
             )
+            return None
         except:
-            print("set geometry failed")
-            return
+            return (("err", "set geometry failed"))
 
-    # if method == "play" and player is not None:
+    elif method == "play":
+        if player is None:
+            return (("err", "no active player"))
 
-        
+        print("play")
+        player[0].play()
+        return None
+
+    elif method == "pause":
+        if player is None:
+            return (("err", "no active player"))
+
+        print("pause")
+        player[0].pause()
+        return None
+
+    elif method == "seek" and (type(message) == int or type(message) == float):
+        if player is None:
+            return (("err", "no active player"))
+
+        print("seek to", message)
+        player[0].setPosition(message)
+        return None
+
+    elif method == "kill":
+        if player is None:
+            return (("err", "no active player"))
+
+        print("stop/remove prev video")
+        os.killpg(os.getpgid(player[1].pid), signal.SIGTERM)
+        sleep(2)
+        player = None
+        return None
+
+    elif method == "s" and (type(message) == int or type(message) == float):
+        if player is None:
+            return (("err", "no active player"))
+
+        print("schedule to", message)
+        scheduler = schedule_play(player[0], message)
+        if scheduler is not None:
+            return None
+        else:
+            return (("err", "cannot schedule (maybe not in sync)"))
+
+    elif method == "cancel":
+        print("cancel schedule")
+        if scheduler is not None:
+            scheduler.set()
+            scheduler = None
+        return None
+    else:
+        return (("err", "unrecognized method or format"))
+
+def on_message(_client, _userdata, msg):
+    res = handle_message(msg)
+    if res is not None:
+        print(res)
+        send_message(res[0], res[1], qos=1)
+    else:
+        # send status
+        pass
 
 '''
-while True:
-    message = str(server.recvfrom(256)[0])[2:-1]
-    print("message:", message)
-    if message[0] == "p":
-        schedule_time = int(message[1:])
-        print("scheduled to", schedule_time)
-        break
-'''
-
-'''
-while get_server_time() < schedule_time:
-    sleep(0.01)
-
 controller.play()
 print("run video")
 
