050e4822322c5b17a7cab37985ca0552dce13eca
aanper <mail@s3f.ru>
2021-12-17 20:39:29 +0500

    mapping in progress

--- server.py
+++ server.py
@@ -16,7 +16,7 @@ from slave_handler import SlaveHandler
 #slave_ids = [x for x in range(1, 40)]
 # slave_ids = [13, 14, 15, 16, 19, 20, 21, 22]
 # slave_ids = [17, 18, 23, 24]
-slave_ids = [3, 4]
+slave_ids = [1, 2]
 
 LOG_WINDOW_HEIGHT = 10
 
